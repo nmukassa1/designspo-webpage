6c0b741c0951421affe0f92f4fa394af98209e8e
nmukassa1 <nmukassa1@gmail.com>
2025-03-17 21:24:37 +0000

    implemented react query

--- src/app/components/EditModal.tsx
+++ src/app/components/EditModal.tsx
@@ -14,14 +14,14 @@ function EditModal({screenshot, handleModal, toggleModal}: EditModalProps) {
     const {id, siteName, tags} = screenshot
     return ( 
         <Modal open={toggleModal} onClose={() => {toggleModal}} sx={{display: "flex", alignItems: 'center', justifyContent: 'center'}}>
-            <div className="w-[45%] h-[75%] bg-[#262626] m-4 p-6 rounded-lg text-white font-bold">
+            <div className="w-[80%] lg:w-[45%] h-[75%] bg-[#262626] m-4 p-6 rounded-lg text-white font-bold">
                 <div className="flex justify-between">
                     <h1 className="text-2xl">{siteName.charAt(0).toUpperCase() + siteName.slice(1)}</h1>
                     <button onClick={handleModal}><X /></button>
                 </div>
                 
                 <ExistingTags tags={tags} screenShotId={id} />
-                <AddNewTag screenShotId={id} />
+                <AddNewTag screenShotId={id} existingTags={tags} />
 
                 <form action="" className="text-center mt-6">
                     <button type="submit" className="bg-red-600 px-4 py-2 rounded-md">Delete Screenshot</button>
--- src/app/components/collectionModal/AddNewTag.tsx
+++ src/app/components/collectionModal/AddNewTag.tsx
@@ -1,29 +1,41 @@
 import { addTagToCollection } from "@/app/mutations";
 import { getTags } from "@/app/queries";
-import { useQuery } from "@tanstack/react-query";
+import { ScreenshotTag } from "@/app/types/types";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Check } from "lucide-react";
 
 const userId = "8c43787a-6332-4f73-8ed3-f00a54f801e4";
-function AddNewTag({screenShotId} : {screenShotId: number}) {
+function AddNewTag({screenShotId, existingTags} : {screenShotId: number, existingTags: ScreenshotTag[]}) {
+
+    const queryClient = useQueryClient();
+
     const {data: tags} = useQuery({
         queryKey: ["tags", userId], 
         queryFn: ({ queryKey }) => getTags(queryKey[1] as string), 
       });
+
+       // Filter out existing tags
+    const filteredTags = tags?.filter((tag) => !existingTags.map(t => t.tagId).includes(tag.id));
     
-    const existingTagIds = tags?.map((tag) => tag.id)
-    const filteredTags = tags?.filter((tag) => !existingTagIds?.includes(tag.id))
+     // Mutation for adding tags
+     const { mutate } = useMutation({
+        mutationFn: (tagId: number) => addTagToCollection(tagId, screenShotId, userId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["tags", userId] }); // Refresh tags after adding
+        },
+    });
       
     return ( 
         <div className="addTags mt-6">
             <h2 className="text-xl">Add Tags</h2>
             <div className="h-[3px] bg-white mt-4 mb-6"></div>
             <ul className="flex flex-col gap-2">
-                {tags?.map((tag) => (
+                {filteredTags?.map((tag) => (
                     <li key={tag.id} className="shrink-0 text-lg flex items-center gap-2  justify-between">
                         <span>{tag.name}</span>
                         <form onSubmit={(e) => {
                             e.preventDefault();
-                            addTagToCollection(tag.id, screenShotId ,userId)
+                            mutate(tag.id);
                         }}>
                             <button type="submit" className="text-green-500"><Check /></button>
                         </form>
