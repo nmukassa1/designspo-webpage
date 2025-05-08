f6c074edd07e4ca0bda61a7e364d0faddff99109
nmukassa1 <nmukassa1@gmail.com>
2025-05-08 21:04:44 +0100

    /

--- src/app/components/sidebar/SkeletonSidebar.tsx
+++ src/app/components/sidebar/SkeletonSidebar.tsx
@@ -1,6 +1,6 @@
 function SkeletonSidebar() {
   return (
-    <aside className="flex flex-col h-full bg-white lg:bg-transparent px-4 w-full lg:w-[18%] text-center lg:text-left animate-pulse">
+    <aside className="flex flex-col h-full bg-white lg:bg-transparent px-4 w-full lg:w-[18%] text-center lg:text-left animate-pulse fixed translate-x-[-100%]">
       <div className="my-4 space-y-4">
         <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto" />
         <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto" />
