fa6115dbaf7d61e875bdadf4331ea416102c91b8
nmukassa1 <nmukassa1@gmail.com>
2025-05-12 18:49:25 +0100

    modal button focus state

--- src/app/components/collectionModal/ExisitngTags.tsx
+++ src/app/components/collectionModal/ExisitngTags.tsx
@@ -1,8 +1,9 @@
 import { useAuthContext } from "@/app/context/AuthContext";
 import { deleteTagFromCollection } from "@/app/mutations";
-import { ScreenshotTag, Tag } from "@/app/types/types";
+import { ScreenshotTag } from "@/app/types/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Trash } from "lucide-react";
+import { useState } from "react";
 
 function ExistingTags({
   tags,
@@ -12,18 +13,22 @@ function ExistingTags({
   screenShotId: number;
 }) {
   const { userId } = useAuthContext();
-
   const queryClient = useQueryClient();
 
+  const [activeTagId, setActiveTagId] = useState<number | null>(null);
+
   const { mutate } = useMutation({
-    mutationFn: (id: number) => {
-      if (!userId) {
-        throw new Error("User ID is required to add a tag.");
-      }
-      return deleteTagFromCollection(id, screenShotId, userId);
+    mutationFn: async (id: number) => {
+      if (!userId) throw new Error("User ID is required to delete a tag.");
+      setActiveTagId(id);
+      return await deleteTagFromCollection(id, screenShotId, userId);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["collections", userId] }); // Refresh tags after adding
+      queryClient.invalidateQueries({ queryKey: ["collections", userId] });
+      setActiveTagId(null);
+    },
+    onError: () => {
+      setActiveTagId(null);
     },
   });
 
@@ -35,11 +40,15 @@ function ExistingTags({
         {tags.map((tag) => (
           <li
             key={tag.tag.id}
-            className="shrink-0 text-lg flex items-center justify-between"
+            className="shrink-0 text-lg flex items-center justify-between relative"
           >
+            {activeTagId === tag.tag.id && (
+              <span className="absolute left-0 top-0 h-full w-full animate-progress bg-[linear-gradient(45deg,_rgba(255,0,0,0.3)_25%,_transparent_25%,_transparent_50%,_rgba(255,0,0,0.3)_50%,_rgba(255,0,0,0.3)_75%,_transparent_75%,_transparent)] bg-[length:40px_40px] z-10 rounded-md" />
+            )}
             <button
               onClick={() => userId && mutate(tag.tag.id)}
-              className="w-full flex justify-between px-4 py-2 hover:bg-white/10 transition-colors duration-200 ease-in-out"
+              className="w-full flex justify-between px-4 py-2 hover:bg-white/10 transition-colors duration-200 ease-in-out relative z-20"
+              disabled={activeTagId !== null}
             >
               <span>{tag.tag.name}</span>
               <div className="text-red-500">
