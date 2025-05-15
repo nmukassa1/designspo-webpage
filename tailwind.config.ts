d7c29b47843b597c0d0d4e6eb8f480c6a74f9bb0
nmukassa1 <nmukassa1@gmail.com>
2025-05-15 22:29:19 +0100

    fetching data longer than usual

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,22 @@
 // tailwind.config.js
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          primary: '#1D4ED8', // Custom primary color
-          secondary: '#FBBF24', // Custom secondary color
-          fontColor: '#555555'
-          // Add more custom colors as needed
-        },
-        spacing: {
-          '128': '32rem', // Custom spacing
-          '144': '36rem',
-          // Add more custom spacing as needed
-        },
-        // Add more customizations as needed
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8", // Custom primary color
+        secondary: "#FBBF24", // Custom secondary color
+        fontColor: "#555555",
+        customBlack: "#262626", // Custom background color
+        // Add more custom colors as needed
       },
+      spacing: {
+        "128": "32rem", // Custom spacing
+        "144": "36rem",
+        // Add more custom spacing as needed
+      },
+      // Add more customizations as needed
     },
-    variants: {},
-    plugins: [],
-  }
+  },
+  variants: {},
+  plugins: [],
+};
