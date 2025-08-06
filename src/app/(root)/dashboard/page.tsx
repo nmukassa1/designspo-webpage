4fad3af7b919bea38672f39a3e04830e5c920f48
nmukassa1 <nmukassa1@gmail.com>
2025-08-06 19:05:21 +0100

    got rid of fetching message

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,8 +1,5 @@
-import { createClient } from "@/app/supabase/superbaseServer";
 import DashboardPageClient from "./DashboardPageClient";
 
 export default async function DashboardPage() {
-  const supabase = await createClient();
-
   return <DashboardPageClient />;
 }
